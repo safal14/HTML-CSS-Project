40b90b953fff4f08c9a8d4c0b1cdfeabd1907e23
Safal Acharya <acharyasafal51@gmial.com>
2025-12-16 21:37:37 +0545

    adding hover effect in images using js

--- Assest/javascript/script.js
+++ Assest/javascript/script.js
@@ -6,3 +6,14 @@ for (let i = 0; i < buttons.length; i++) {
         alert("Order placed");
     });
 }
+// images hover effects
+const img = document.getElementsByTagName("img");
+for(let i = 0; i < img.length; i++){
+    img[i].addEventListener("mouseover",function(){
+         img[i].style.transform = "scale(1.1)";
+    img[i].style.transition = "0.3s";
+    });
+img[i].addEventListener("mouseout", function () {
+        img[i].style.transform = "scale(1)";
+});
+}
